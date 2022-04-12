29fb5a9d2ee64a01cb70fa30712c09adc2a394e8
Vendll <utto.vendel@icloud.com>
2022-04-12 14:22:55 +0200

    keresés

--- components/navigation/Navbar.js
+++ components/navigation/Navbar.js
@@ -390,7 +390,7 @@ const Navbar = () => {
                 <div className="relative z-0 flex-1 px-2 flex items-center justify-center sm:absolute sm:inset-0">
                   <div className="w-full sm:w-96 ">
                     <label htmlFor="search" className="sr-only">
-                      Search
+                      Keresés
                     </label>
                     <div className="relative">
                       <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
@@ -403,7 +403,7 @@ const Navbar = () => {
                         id="search"
                         name="search"
                         className="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-epgreen focus:border-epgreen sm:text-sm"
-                        placeholder="Search"
+                        placeholder="Keresés"
                         type="search"
                       />
                     </div>
